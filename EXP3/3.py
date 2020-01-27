9df1d1a81cdb5429f791bf310584f5ef959ae2c7
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2020-01-27 09:07:30 +0530

    Create 3.py

---
+++ EXP3/3.py
@@ -0,0 +1,8 @@
+import random
+
+rand_nos = float(sum([random.randrange(0, 10), random.randrange(0, 100)])
+sun = float(input("enter the sum")
+if sun == rand_nos:
+  print("OK")
+else:
+  print("NOT OK")
