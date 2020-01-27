f0bbc185832ec91cee1e4e862aa6f3fe0dcaf403
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2020-01-27 09:17:18 +0530

    Create 5.py

---
+++ EXP3/5.py
@@ -0,0 +1,10 @@
+w1, p1 = input("wieght of the rice 1"), input("price of the rice 1")
+w2, p2 = input("wieght of the rice 2"), input("price of the rice 2")
+
+r1 = p1/w1
+r2 = p2/w2
+
+if r1 > r2:
+  print("tice 1")
+else:
+  print("rivce 2")
