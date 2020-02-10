8997e23d12d82092d87cbff45c62832c1ea76b4f
am1O <48621503+am1O@users.noreply.github.com>
2020-02-10 09:50:16 +0530

    Add files via upload

---
+++ EXP7/exp7_8.py
@@ -0,0 +1,4 @@
+test_list = [(2, 3), (4, 7), (8, 11), (3, 6)] 
+print ("The original list is : " + str(test_list))  
+res = min(test_list)[1]
+print ("The min and max of index 2 :  " +  str(res)) 
---
+++ EXP7/exp7_9.py
@@ -0,0 +1,4 @@
+test_dict = {} 
+print("The original dictionary : " + str(test_dict))  
+res = not bool(test_dict) 
+print("Is dictionary empty ? : " + str(res)) 
